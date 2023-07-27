763b8e4daefd7e30f55d285fd54d96e813a412a0
Quetellen <quetellen.bertazo@gmail.com>
2023-07-27 16:10:19 -0700

    clients

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Client } from '../client.model';
 
 @Component({
   selector: 'app-client',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent {
+ client: Client [] = [];
+ 
+ constructor(){
+  this.client = [ {
+    name: 'Olivia ',
+    username: 'Olivia',
+    email: 'oliviaM24@gmail.com',
+    address: '7896 Toradora',
+    contact: '612-551-8907'
+  },
+  {
+    name: 'Jose ',
+    username: 'Jose',
+    email: 'Jose24@gmail.com',
+    address: '789 New Horizon',
+    contact: '612-551-8907'
+  }
 
+]
+ }
 }
