1e358e85e960cd01f6a290999679deb999774238
Quetellen <quetellen.bertazo@gmail.com>
2023-07-26 20:32:54 -0700

    css color

--- src/app/client-meeting/client-meeting.component.ts
+++ src/app/client-meeting/client-meeting.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-client-meeting',
   templateUrl: './client-meeting.component.html',
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -23,6 +23,7 @@ export class LoginComponent {
     this.userAuthenticated.emit(true);
   }
 
-  
+   
+
 
 }
